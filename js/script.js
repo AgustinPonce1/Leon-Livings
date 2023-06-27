0a37f1688b3e114db0796f3567c24ee490cac309
AgustinPonce1 <agustinlucaspdl@gmail.com>
2023-06-27 18:28:33 -0300

    SECCION SUCURSALES AÑADIDA Y SE ARREGLARON LOS LINKS INTERNOS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// EFECTO AL BAJAR PAGINA PRINCIPAL //
+
 window.addEventListener("scroll", function() {
   var scrollPos = window.scrollY;
   var elementOffset = document.querySelector(".scroll-effect").offsetTop;
@@ -7,6 +9,9 @@ window.addEventListener("scroll", function() {
     document.querySelector(".scroll-effect").classList.add("active");
   }
 });
+
+
+// CARRUSEL PAGINA PRINCIPAL //
 
 const slideItems = document.querySelectorAll('.slide-item');
 const slideControls = document.querySelectorAll('.slide-control');
@@ -42,3 +47,14 @@ slideControls.forEach(control => {
 
 mostrarSlide(); // Mostrar el primer slide al cargar la página
 iniciarCarrusel(); // Iniciar el carrusel automáticamente
+
+
+// REDIGIR PAGINA LOCALES //
+
+function redirigir() {
+    window.location.href = "https://www.google.com.ar/maps/place/Cdad.+Jardin+Lomas+de+Palomar,+Provincia+de+Buenos+Aires/@-34.6046688,-58.6045142,13.5z/data=!4m6!3m5!1s0x95bcb9ab4a5771b3:0x690cbb88f054e680!8m2!3d-34.5962102!4d-58.5913678!16s%2Fm%2F03hmlh0?entry=ttu"; 
+}
+
+function redirigir2() {
+    window.location.href = "https://www.google.com.ar/maps/place/Cdad.+Jardin+Lomas+de+Palomar,+Provincia+de+Buenos+Aires/@-34.6046688,-58.6045142,13.5z/data=!4m6!3m5!1s0x95bcb9ab4a5771b3:0x690cbb88f054e680!8m2!3d-34.5962102!4d-58.5913678!16s%2Fm%2F03hmlh0?entry=ttu"; 
+}
