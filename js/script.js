9b7af4672175838eb3e14b4653b09b8f25d6e911
AgustinPonce1 <agustinlucaspdl@gmail.com>
2023-07-02 12:37:34 -0300

    RESPONSIVE A 950PX x --, AGREGADO MENU HAMBURGESA APARTIR DE ESTA RESOLUCION

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ window.addEventListener("scroll", function() {
 });
 
 
+
+
 // CARRUSEL PAGINA PRINCIPAL //
 
 const slideItems = document.querySelectorAll('.slide-item');
@@ -49,6 +51,8 @@ mostrarSlide(); // Mostrar el primer slide al cargar la página
 iniciarCarrusel(); // Iniciar el carrusel automáticamente
 
 
+
+
 // REDIGIR PAGINA LOCALES //
 
 function redirigir() {
@@ -58,3 +62,39 @@ function redirigir() {
 function redirigir2() {
     window.location.href = "https://www.google.com.ar/maps/place/Cdad.+Jardin+Lomas+de+Palomar,+Provincia+de+Buenos+Aires/@-34.6046688,-58.6045142,13.5z/data=!4m6!3m5!1s0x95bcb9ab4a5771b3:0x690cbb88f054e680!8m2!3d-34.5962102!4d-58.5913678!16s%2Fm%2F03hmlh0?entry=ttu"; 
 }
+
+
+
+
+/* SISTEMA DE MENU HAMBURGESA */
+
+document.addEventListener("DOMContentLoaded", function() {
+  var menuIcon = document.querySelector(".menu-icon");
+  var header = document.getElementById("main-header");
+
+  menuIcon.addEventListener("click", function() {
+      header.classList.toggle("show-nav");
+  });
+
+  window.addEventListener("resize", function() {
+      if (window.innerWidth > 950) {
+          header.classList.remove("show-nav");
+      }
+  });
+});
+
+
+
+
+/* DROPDOWN-CONTENT DEL MENU HAMBURGESA */
+
+document.addEventListener('DOMContentLoaded', function () {
+  var dropbtn = document.querySelector('.dropbtn');
+  var dropdown = document.querySelector('.dropdown');
+  var dropdownContent = document.querySelector('.dropdown-content');
+
+  dropbtn.addEventListener('click', function () {
+    dropdown.classList.toggle('active');
+    dropdownContent.style.display = dropdown.classList.contains('active') ? 'block' : 'none';
+  });
+});
